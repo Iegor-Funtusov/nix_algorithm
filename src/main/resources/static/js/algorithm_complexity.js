3e7d2ecfd1b3caed56e3898e4a8a63ebb5a86ace
funtushan@gmail.com <funtushan@gmail.com>
2020-07-01 10:42:17 +0300

    add algorithm complexity process

--- src/main/resources/static/js/algorithm_complexity.js
+++ src/main/resources/static/js/algorithm_complexity.js
@@ -1,27 +1,98 @@
-function orderConstant() {
-    submitRequest('inputOrderConstant', 'orderConstant', 'resultOrderConstant');
+let modalData;
+let modalDataType;
+
+document.getElementById("modalOrderConstant").addEventListener("click", setModalDataTypeConstant);
+document.getElementById("modalOrderLinear").addEventListener("click", setModalDataTypeLinear);
+document.getElementById("modalOrderLogarithmic").addEventListener("click", setModalDataTypeLogarithmic);
+document.getElementById("modalOrderLinearLogarithmic").addEventListener("click", setModalDataTypeLinearLogarithmic);
+
+function setModalDataTypeConstant() {
+    modalDataType = 'modalOrderConstant';
+    loadModalData();
+    initModalData();
 }
 
-function orderLinear() {
-    submitRequest('inputOrderLinear', 'orderLinear', 'resultOrderLinear');
+function setModalDataTypeLinear() {
+    modalDataType = 'modalOrderLinear';
+    loadModalData();
+    initModalData();
 }
 
-function orderLogarithmic() {
-    submitRequest('inputOrderLogarithmic', 'orderLogarithmic', 'resultOrderLogarithmic');
+function setModalDataTypeLogarithmic() {
+    modalDataType = 'modalOrderLogarithmic';
+    loadModalData();
+    initModalData();
 }
 
-function submitRequest(inputId, path, resultId) {
-    let data = document.getElementById(inputId).value;
+function setModalDataTypeLinearLogarithmic() {
+    modalDataType = 'modalOrderLinearLogarithmic';
+    loadModalData();
+    initModalData();
+}
+
+function runAlgorithmComplexity() {
+    if (modalDataType) {
+        switch (modalDataType) {
+            case 'modalOrderConstant' : {
+                submitRequest('orderConstant');
+            } break;
+            case 'modalOrderLinear' : {
+                submitRequest('orderLinear');
+            } break;
+            case 'modalOrderLogarithmic' : {
+                submitRequest('orderLogarithmic');
+            } break;
+        }
+    }
+}
+
+function submitRequest(path) {
+    let data = document.getElementById('inputOrder').value;
     jQuery.ajax({
         type: "POST",
         contentType: "application/json",
         url: "/algorithm_complexity/" + path,
         data: data,
         success: function (result) {
-            document.getElementById(resultId).innerHTML = result.time;
+            document.getElementById('resultOrder').value = result.time;
         },
         error: function () {
             return "error";
         }
     });
+}
+
+function loadModalData() {
+    if (!modalData) {
+        $.getJSON('/json/algorithm_complexity_data_modal.json', function (data) {
+            modalData = data;
+            initModalData();
+        });
+    }
+}
+
+function initModalData() {
+    if (modalData) {
+        let data = modalData.data;
+        let exampleModalLabel = document.getElementById('exampleModalLabel');
+        let modalBodyId = document.getElementById('modalBodyId');
+        switch (modalDataType) {
+            case 'modalOrderConstant' : {
+                exampleModalLabel.innerHTML = data.orderConstant.modalTitle;
+                modalBodyId.innerHTML = data.orderConstant.modalBody;
+            } break;
+            case 'modalOrderLinear' : {
+                exampleModalLabel.innerHTML = data.orderLinear.modalTitle;
+                modalBodyId.innerHTML = data.orderLinear.modalBody;
+            } break;
+            case 'modalOrderLogarithmic' : {
+                exampleModalLabel.innerHTML = data.orderLogarithmic.modalTitle;
+                modalBodyId.innerHTML = data.orderLogarithmic.modalBody;
+            } break;
+            case 'modalOrderLinearLogarithmic' : {
+                exampleModalLabel.innerHTML = data.orderLinearLogarithmic.modalTitle;
+                modalBodyId.innerHTML = data.orderLinearLogarithmic.modalBody;
+            } break;
+        }
+    }
 }
